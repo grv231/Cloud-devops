1b4a6e5e953059de8258c01bfc8ac027de8cf02e
Gaurav-Tripathi <grv231@gmail.com>
2018-01-28 21:14:09 -0600

    Adding LAMP playbook and roles for PHP and Apache service

---
+++ roles/php/files/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Welcome to Devops practices on AWS </title>
+</head>
+<body>
+	<h1><?php echo "Welcome to the Devops AWS php page" ?></h1>
+</body>
+</html>
